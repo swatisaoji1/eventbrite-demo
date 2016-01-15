89c6ffb291181fac0f700eecc49c5959eed048e6
Swati <swatisaoji1@gmail.com>
2016-01-15 13:42:37 -0800

    Created an app events and wrote initial basic unit tests

---
+++ events/tests.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from django.http import HttpRequest
+
+from events.views import  home_page
+
+# Create your tests here.
+
+class homePageViewTest(TestCase):
+
+	def test_home_page_html(self):
+		request= HttpRequest()
+		response = home_page(request)
+		self.assertIn('<title>EventBrite Events</title>', response.content)
